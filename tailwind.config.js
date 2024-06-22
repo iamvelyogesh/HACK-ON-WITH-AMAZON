837aee322ddf2072490d4a36acc3e0a6d5d0b1da
VEL YOGESH <113898735+iamvelyogesh@users.noreply.github.com>
2024-06-22 21:31:29 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust the path to your components if needed
-    theme: {
-      extend: {}, // Add your custom theme extensions here
-    },
-    plugins: [], // Add any custom plugins here
-  };
