53e92c4162fb56695acc0f3f82b430e48bf3f87b
VEL YOGESH <113898735+iamvelyogesh@users.noreply.github.com>
2024-06-22 21:32:08 +0530

    Add files via upload

---
+++ Amazon Shopping/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust the path to your components if needed
+    theme: {
+      extend: {}, // Add your custom theme extensions here
+    },
+    plugins: [], // Add any custom plugins here
+  };
